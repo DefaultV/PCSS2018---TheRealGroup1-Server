0176250b8988817c05f9bfad80abe28c9d8be3f0
JannickD <dozermailjd@gmail.com>
2018-09-20 14:42:41 +0200

    Initialize with server basics

---
+++ src/Server.java
@@ -0,0 +1,23 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Server {
+
+  DataInputStream iStream;
+  DataOutputStream oStream;
+  List<String> client_ids;
+
+  public static void main(String[] args){
+
+  }
+
+  void ClientHandler(){}
+
+  void CreateLobby(){}
+
+  void SetLobby(){}
+
+  void ExitLobby(){}
+
+}
