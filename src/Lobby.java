05021517d3843aaad8bcea5f8fd6d8fa54d1ea38
JannickD <dozermailjd@gmail.com>
2018-09-28 11:59:50 +0200

    New branch with lobby

---
+++ src/Lobby.java
@@ -0,0 +1,17 @@
+public class Lobby{
+  private String id;
+  private String name;
+
+  public Lobby(String id, String name){
+      this.id = id;
+      this.name = name;
+  }
+
+  public String GetId(){
+    return this.id;
+  }
+
+  public String GetLobbyName(){
+    return this.name;
+  }
+}
