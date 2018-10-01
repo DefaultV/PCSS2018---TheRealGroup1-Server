2a90ed68141ede9c60aabbd9ee38b2187753980f
JannickD <dozermailjd@gmail.com>
2018-10-01 16:41:15 +0200

    Game-Handling

--- src/Server/Game.java
+++ src/Server/Game.java
@@ -1,9 +1,16 @@
 package Server;
 import java.util.List;
+import java.lang.Math;
 import Client.Player;
 
 enum Emojis {
 	lol, talking, shouting, whisper
+}
+
+enum Action{
+  TALKING,
+  JOINING,
+  LEAVING
 }
 
 public class Game {
@@ -27,4 +34,50 @@ public class Game {
 	public boolean isPlaying(){
 		return playing;
 	}
+
+  public void Broadcast(List<Player> l_ply, String txt){
+    for(Player ply : l_ply){
+      System.out.format("%s", txt);
+      //TODO
+      //Send text to player
+      //Server.sendtext(txt)
+    }
+  }
+
+  public void SetPosition(Player ply, int[] pos){
+    this.playerList.get(playerList.indexOf(ply)).SetLocation(pos);
+    //Update position on map and give info to clients
+    //TODO
+  }
+
+  public void SetAction(Player ply, Action act){
+    String form = "";
+    switch(act){
+      case TALKING:
+        form += String.format("%s is talking...", ply.GetNickName());
+        break;
+      case JOINING:
+        form += String.format("%s is joining...", ply.GetNickName());
+        break;
+      case LEAVING:
+        form += String.format("%s is leaving...", ply.GetNickName());
+        break;
+    }
+    Broadcast(playerList, form);
+    //Update action on map and give info to clients
+    //TODO
+  }
+
+  public void SetSaying(Player ply, String text){
+    //if range between p1 and p2 <= 5
+    for (Player oply : playerList){
+      //if (Math.sqrt(Math.pow((ply.GetLocation[0] - oply.GetLocation[0]), 2))+Math.pow((ply.GetLocation[1] - oply.GetLocation[1]), 2)){
+      //TODO euclidean distance to player
+      //}
+    }
+    //make icon instead TODO
+    //Update saying on map and give info to clients
+    String form = String.format("%s says: %s", ply.GetNickName(), text);
+    Broadcast(playerList, form);
+  }
 }
