2a48661427ce00c8e8ef6fdcd9507c2dbacfa44d
SebastianYoung <36990316+SebastianYoung@users.noreply.github.com>
2018-10-04 18:05:31 +0200

    Added server stuff so it should work

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -16,19 +16,37 @@ public class Server {
 		int port = 6690;
 
 		//Creating the serversocket
-			ServerSocket ourServerSocket = new ServerSocket(port);
+		ServerSocket ourServerSocket = new ServerSocket(port);
+		//System.out.println("Please Enter your username");
+		
+		//System.out.println("Please Write /Create Server + Your_ServerName");
+		
+		
+		
+		//if (input.nextLine("Create Server "))
+		//{
+			//System.out.println("You wrote the write thing");
+		//}
+		//double radius = input.nextDouble();
+
+		//System.out.println(2 * radius);
+
+
+	// Accepts the request that the client class is sending
+	String clientSentence;
+	String serverMessage;
+	Socket clientSocket = ourServerSocket.accept();
+	DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+	DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+	clientSentence = in.readUTF();
+	out.writeUTF("Welcome " + clientSentence + "!");
+	double annualIR = in.readDouble();
+	int numberOfYears = in.readInt();
+	double loanAmount = in.readDouble();
+
 	
-		// Accepts the request that the client class is sending
-		Socket clientSocket = ourServerSocket.accept();
-		Scanner serverScanner = new Scanner(clientSocket.getInputStream());
-		
-		int number = 0, temp;
-		temp = number * 2;
-		
-		PrintStream player = new PrintStream(clientSocket.getOutputStream());
-		player.println(temp);
-		
-		
+	
+
 
 	}
 
