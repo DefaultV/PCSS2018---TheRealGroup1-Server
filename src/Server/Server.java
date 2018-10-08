8f31e330c750640f31f83280e3cb1670a2b0e2cd
JannickD <dozermailjd@gmail.com>
2018-10-08 12:16:39 +0200

    Merge branches + fix tabs

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -7,49 +7,49 @@ import Client.Player;
 
 public class Server {
 
-	DataInputStream iStream;
-	DataOutputStream oStream;
-	List<Player> client_ids;
+  DataInputStream iStream;
+  DataOutputStream oStream;
+  List<Player> client_ids;
 
-	public static void main(String[] args) throws IOException {
-		//Specifying the serverSocket port number
-		int port = 6690;
+  public static void main(String[] args) throws IOException {
+    //Specifying the serverSocket port number
+    int port = 6690;
 
-		//Creating the serversocket
-		ServerSocket ourServerSocket = new ServerSocket(port);
-		//System.out.println("Please Enter your username");
-		
-		//System.out.println("Please Write /Create Server + Your_ServerName");
-		
+    //Creating the serversocket
+    ServerSocket ourServerSocket = new ServerSocket(port);
+    //System.out.println("Please Enter your username");
 
-	// Accepts the request that the client class is sending
-	String clientSentence;
-	String serverMessage;
-	Socket clientSocket = ourServerSocket.accept();
-	DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-	DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-	clientSentence = in.readUTF();
-	out.writeUTF("Welcome " + clientSentence + "!");
-	double doubles = in.readDouble();
-	int integers = in.readInt();
-
-	
-	
+    //System.out.println("Please Write /Create Server + Your_ServerName");
 
 
-	}
+    // Accepts the request that the client class is sending
+    String clientSentence;
+    String serverMessage;
+    Socket clientSocket = ourServerSocket.accept();
+    DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+    DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+    clientSentence = in.readUTF();
+    out.writeUTF("Welcome " + clientSentence + "!");
+    double doubles = in.readDouble();
+    int integers = in.readInt();
 
-	void ClientHandler() {
-	}
 
-	void CreateLobby() {
-		
-	}
 
-	void SetLobby(Player player) {
-		
-	}
 
-	void ExitLobby() {
-	}
+
+  }
+
+  void ClientHandler() {
+  }
+
+  void CreateLobby() {
+
+  }
+
+  void SetLobby(Player player) {
+
+  }
+
+  void ExitLobby() {
+  }
 }
